f13c89a02aa674a963e057dedd25d07c5eb6ccde
Erik Pukinskis <erik@snowedin.net>
2017-03-17 12:52:51 -0700

    update deps

--- tap-away.js
+++ tap-away.js
@@ -1,76 +1,71 @@
+var library = require("module-library")(require)
 
-if (require) {
-  var library = require("nrtv-library")(require)
-  module.exports = library.export(
-    "tap-away",
-    ["web-element", "function-call"],
-    generator
-  )
-} else {
-  var tapAway = generator(element)
-}
+module.exports = library.export(
+  "tap-away",
+  ["web-element", "function-call"],
+  function generator(element, functionCall) {
 
-function generator(element, functionCall) {
+    // CATCH DEM TAPS
 
-  // CATCH DEM TAPS
+    function tapOutCatcher(child, callback) {
 
-  function tapOutCatcher(child, callback) {
+      var catcher = element(
+        ".tap-catcher",
+        {
+          onclick: tapOutScript(callback)
+        },
+        element.style({
+          "position": "fixed",
+          "top": "0",
+          "left": "0",
+          "width": "100%",
+          "height": "100%",
+          "z-index": "1000"
+        }),
+        child
+      )
 
-    var catcher = element(
-      ".tap-catcher",
-      {
-        onclick: tapOutScript(callback)
-      },
-      element.style({
-        "position": "fixed",
-        "top": "0",
-        "left": "0",
-        "width": "100%",
-        "height": "100%",
-        "z-index": "1000"
-      }),
-      child
-    )
+      function tapOutScript(callback) {
+        var singleton = typeof tapAway == "undefined" ? "library.get(\"tap-away\")" : tapAway
 
-    function tapOutScript(callback) {
-      var singleton = typeof tapAway == "undefined" ? "library.get(\"tap-away\")" : tapAway
+        return functionCall(singleton).withArgs(functionCall.raw("event"), callback).evalable()
+      }
 
-      return functionCall(singleton).withArgs(functionCall.raw("event"), callback).evalable()
+      catcher.assignId()
+
+      catcher.onTapOut =
+        function(callback) {
+          document.getElementById(this.id).setAttribute("onclick", tapOutScript(callback))
+        }
+
+      catcher.show =
+        function() {
+          document.getElementById(this.id).style.display = "block"
+        }
+
+      catcher.hide =
+        function() {
+          document.getElementById(this.id).style.display = "none"
+        }
+
+      return catcher
     }
 
-    catcher.assignId()
-
-    catcher.onTapOut =
-      function(callback) {
-        document.getElementById(this.id).setAttribute("onclick", tapOutScript(callback))
+    function tapOut(event, callback) {
+      var catcherElement = event.target
+      
+      if (!catcherElement.classList.contains("tap-catcher")) {
+        return
       }
 
-    catcher.show =
-      function() {
-        document.getElementById(this.id).style.display = "block"
-      }
+      catcherElement.style.display = "none"
 
-    catcher.hide =
-      function() {
-        document.getElementById(this.id).style.display = "none"
-      }
-
-    return catcher
-  }
-
-  function tapOut(event, callback) {
-    var catcherElement = event.target
-    
-    if (!catcherElement.classList.contains("tap-catcher")) {
-      return
+      callback && callback()
     }
 
-    catcherElement.style.display = "none"
+    tapOut.catcher = tapOutCatcher
 
-    callback && callback()
+    return tapOut
   }
-
-  tapOut.catcher = tapOutCatcher
-
-  return tapOut
-}
+)
+  
